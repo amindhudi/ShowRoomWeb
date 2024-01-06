e2252b51f2b77e6bd1bc4eb0c86b7a7c8e6de532
do_muhammad.amin <muhammad.amin@digitalomega.com>
2024-01-06 21:11:19 +0500

    Save changes

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -8,8 +8,8 @@ import { ArrowRight } from "lucide-react"
 import Link from "next/link"
 import { useForm } from "react-hook-form"
 import {zodResolver} from '@hookform/resolvers/zod'
-import {z} from 'zod'
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator"
+import { trpc } from "@/trpc/client"
 const Page =()=>{
  
   const {
@@ -19,6 +19,11 @@ const Page =()=>{
   } = useForm<TAuthCredentialsValidator>({
     resolver:zodResolver(AuthCredentialsValidator),
   });
+
+//const { data } = trpc.anyApiRoute.useQuery()
+
+
+
   const onSubmit=({email, password}: TAuthCredentialsValidator) => {
    //send data to the server
    
--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import path from 'path'
 import type{InitOptions} from 'payload/config'
-import payload from 'payload'
+import payload, { Payload } from 'payload'
 
 
 dotenv.config({
@@ -22,7 +22,7 @@ interface Args{
 
 export const getPayloadClient = async ({
     initOptions,
-}: Args) =>{
+}: Args={}): Promise<Payload> =>{
 if(!process.env.PAYLOAD_SECRET){
     throw new Error('PAYLOAD_SECRET is missing')
 }
